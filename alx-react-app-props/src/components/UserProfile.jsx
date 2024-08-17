0496347110f8e962b9388798e352171c64956be4
Esmael Ali <154881690+esmizz@users.noreply.github.com>
2024-08-17 19:24:51 +0300

    Add files via upload

---
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -0,0 +1,14 @@
+import React, { useContext } from "react";
+import UserContext from "./UserContext";
+
+const UserProfile = (props) => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
